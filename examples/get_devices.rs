bd4bcb771ac04b3a51e5bd2e953dacbeb3ced0a3
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-08-13 16:36:03 -0500

    Partition audio and video devices

--- examples/get_devices.rs
+++ examples/get_devices.rs
@@ -1,9 +1,31 @@
-use livekit_gstreamer::get_devices_info;
+use livekit_gstreamer::{get_devices_info, MediaDeviceInfo};
 
 fn main() {
     gstreamer::init().unwrap();
     let devices = get_devices_info();
-    for device_info in devices {
+
+    let (video_devices, audio_devices): (Vec<MediaDeviceInfo>, Vec<MediaDeviceInfo>) = devices
+        .into_iter()
+        .partition(|device| device.device_class == "Video/Source");
+
+    println!("Video Devices:");
+    for device_info in video_devices {
+        println!(
+            "============== Media Device Info ({:?}|{:?}) ==============",
+            device_info.display_name, device_info.device_class
+        );
+        println!("Device path: {}", device_info.device_path);
+        println!("Device name: {}", device_info.display_name);
+        println!("Device class: {}", device_info.device_class);
+        println!("Capabilities:");
+        for capability in device_info.capabilities {
+            println!("  {:?}", capability);
+        }
+        println!("============== End Media Device Info ==============");
+    }
+    println!("\n------------------------------------------------------\n");
+    println!("Audio Devices:");
+    for device_info in audio_devices {
         println!(
             "============== Media Device Info ({:?}|{:?}) ==============",
             device_info.display_name, device_info.device_class
